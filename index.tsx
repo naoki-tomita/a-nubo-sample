3e10a912e851aa11b767a1a82fbb9050b9d1ebf1
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2021-04-29 16:23:44 +0900

    improve design

--- index.tsx
+++ index.tsx
@@ -7,8 +7,9 @@ import marked from "marked";
 const Index: VFC<{ list: string[] }> = ({ list }) => {
   return (
     <div>
+      <h1>Markdown blog</h1>
       <ul>
-        {list.map(it => <li>{list}</li>)}
+        {list.map((it, i) => <li key={i}><a href={`/${it}`}>{list}</a></li>)}
       </ul>
     </div>
   );
@@ -24,24 +25,32 @@ async function renderIndex() {
 }
 
 async function renderDoc(filename: string) {
-  const doc = await readFile(`./docs/${filename}.md`).then(it => it.toString("utf-8")).then(md => marked(md));
-  const template = await readFile(`./docs.template.html`).then(it => it.toString("utf-8"));
-  const html = template.replace("__DOCS__", doc);
-  return html;
+  try {
+    const doc = await readFile(`./docs/${filename}.md`).then(it => it.toString("utf-8")).then(md => marked(md)).catch(() => "404")
+    const template = await readFile(`./docs.template.html`).then(it => it.toString("utf-8"));
+    const html = template.replace("__DOCS__", doc);
+    return html;
+  } catch (e) {
+    throw e;
+  }
 }
-
-
 
 function main() {
   const server = createServer(async (req, res) => {
+    console.log(`[${Date.now()}]: Request ${req.url}`);
     try {
       if (req.url === "/") {
         res.writeHead(200, { "content-type": "text/html" }).end(await renderIndex());
+        console.log(`[${Date.now()}]: Response index`);
+      } else if (req.url === "/favicon.ico") {
+        res.writeHead(404).end("");
+        console.log(`[${Date.now()}]: Response favicon.ico`);
       } else {
         res.writeHead(200, { "content-type": "text/html" }).end(await renderDoc(req.url?.slice(1) ?? ""));
+        console.log(`[${Date.now()}]: Response doc ${req.url}`);
       }
     } catch (e) {
-      res.writeHead(200).end(e);
+      res.writeHead(500).end(e);
     }
   });
   server.listen(process.env.PORT)
